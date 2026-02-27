ec76fe368a068d80210cd937ba98e1a8db1afba0
Timileyin Okediya <105449068+Awo-Oosha@users.noreply.github.com>
2026-02-27 16:46:13 +0100

    fix: num_predict and safety check fix

--- llm.py
+++ llm.py
@@ -4,11 +4,11 @@ from langchain_ollama import ChatOllama
 class LLM:
     def __init__(
         self,
-        model: str = "qwen2.5:1.5b",
+        model: str = "qwen3:1.7b",
         temperature: float = 0.2,
         num_ctx: int = 1024,
         top_p: float = 0.9,
-        num_predict: int = 128,
+        num_predict: int = 265,
         base_url: str = "http://172.23.16.1:11434",
     ):
         self.llm = ChatOllama(
--- nodes/safety_node.py
+++ nodes/safety_node.py
@@ -20,39 +20,37 @@ BLOCKED_KEYWORDS = [
 
 def safety_check(llm: LLM):
     def _node(state: AgentState):
+
+        blocked_keywords_str = "\n".join(f"- {kw}" for kw in BLOCKED_KEYWORDS)
+        task = state["messages"][-1].content
+
+        messages = SAFETY_PROMPT.format_messages(
+            task=task,
+            blocked_keywords=blocked_keywords_str,
+            format_instructions=SAFETY_PROMPT_FORMAT_INSTRUCTIONS,
+        )
+
+        result = llm.invoke(messages)
+
+        raw = result.content
+        print(raw)
         try:
-            blocked_keywords_str = "\n".join(f"- {kw}" for kw in BLOCKED_KEYWORDS)
-            task = state["messages"][-1].content
-
-            messages = SAFETY_PROMPT.format_messages(
-                task=task,
-                blocked_keywords=blocked_keywords_str,
-                format_instructions=SAFETY_PROMPT_FORMAT_INSTRUCTIONS,
-            )
-
-            result = llm.invoke(messages=messages)
-
-            raw = result.content
-            cleaned = re.sub(r"```json|```", "", raw).strip()
-
-            content = json.loads(cleaned)
-            print(content)
-
-            return {
-                **state,
-                "user_input": state["messages"][-1].content,
-                "safe": content.get("is_safe", False),
-            }
-
+            content = json.loads(raw)
         except Exception as e:
             print("Safety parsing failed:", e)
-            print("Raw output:", result.content if "result" in locals() else "No result")
+            print("Raw output:", raw)
 
-            # Fail closed
+            # Fail CLOSED
             return {
                 **state,
                 "safe": False,
-                "safety_reason": "Safety parsing failed"
+                "safety_reason": "Safety parsing failed",
             }
+
+        return {
+            **state,
+            "safe": content.get("is_safe", False),
+            "safety_reason": content.get("reason", ""),
+        }
 
     return _node
